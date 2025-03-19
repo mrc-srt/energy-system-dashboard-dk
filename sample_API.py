8be184870379b351924644101c3faec92ea47f4d
Marco Saretta <92331447+mrc-srt@users.noreply.github.com>
2025-03-19 08:20:53 +0100

    fix sort order api.py

--- sample_API.py
+++ sample_API.py
@@ -1,27 +1,27 @@
 import requests
-import json
 import pandas as pd
 
 
-API_URL = 'https://api.energidataservice.dk/dataset/DeclarationProduction'
-PARAMS = {
-    "start": "2025-01-01",
-    "end": "2025-01-27",
-    "limit": 0,
-    "filter": '{ "PriceArea": "DK2" }'
-}
+api_url = 'https://api.energidataservice.dk/dataset/ElectricityProdex5MinRealtime'
+
+query_params = {"start": "2025-01-01",
+          "end": "2025-01-27",
+          "limit": 0,
+          "filter": '{ "PriceArea": "DK2" }',
+          "sort" : "Minutes5UTC ASC"
+          }
 
 def fetch_data(url, params):
     """Fetch data from API with error handling."""
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(api_url, params=query_params, timeout=1000)
         response.raise_for_status()  # Raise error for bad responses (4xx, 5xx)
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"API request failed: {e}")
         return None
 
-result = fetch_data(API_URL, PARAMS)
+result = fetch_data(api_url, query_params)
 
 # Extract records
 records = result.get("records", [])
